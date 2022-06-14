6720fdb7ca6992109ec3e08edf3066bc3f587bb4
17marsel17 <46229167+17marsel17@users.noreply.github.com>
2022-06-14 17:26:27 +0300

    Middleware для 404

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {PORT} from './config.js'
 import {router as booksRouter} from './routes/booksRouter.js'
 import {router as userRouter} from './routes/userRouter.js'
 import {logger} from './middleware/logger.js'
+import {error as error404} from './middleware/err-404.js'
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(logger);
 
 app.use('/api/books', booksRouter);
 app.use('/api/user', userRouter);
+
+app.use('error404');
 
 app.listen(PORT);
